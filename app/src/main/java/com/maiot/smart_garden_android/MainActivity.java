be1414aed56dd3b07e7952c35536909a864e7f2c
Ludovico Pestarino <ludovicopestarino00@gmail.com>
2023-07-06 21:55:29 +0200

    changed main activity xml

--- app/src/main/java/com/maiot/smart_garden_android/MainActivity.java
+++ app/src/main/java/com/maiot/smart_garden_android/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.plant_editor);
+        setContentView(R.layout.plant_view);
     }
 }
