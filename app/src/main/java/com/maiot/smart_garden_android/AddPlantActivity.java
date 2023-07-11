924ab6105f17eea8890542331dc7c722f463f1b4
Ludovico Pestarino <ludovicopestarino00@gmail.com>
2023-07-11 18:49:32 +0200

    removed unused files

--- app/src/main/java/com/maiot/smart_garden_android/AddPlantActivity.java
+++
@@ -1,7 +0,0 @@
-package com.maiot.smart_garden_android;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class AddPlantActivity extends AppCompatActivity {
-
-}
--- app/src/main/java/com/maiot/smart_garden_android/ViewPlantActivity.java
+++
@@ -1,6 +0,0 @@
-package com.maiot.smart_garden_android;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class ViewPlantActivity extends AppCompatActivity {
-}
